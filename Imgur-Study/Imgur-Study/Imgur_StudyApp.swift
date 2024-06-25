9b404a6514ad87d7d2df8bb271d46780f1ad24d0
lucasfrancisco <lucasfranciscosp7@gmail.com>
2024-06-25 11:28:13 -0300

    added base app

---
+++ Imgur-Study/Imgur-Study/Imgur_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Imgur_StudyApp.swift
+//  Imgur-Study
+//
+//  Created by Lucas Francisco on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Imgur_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
