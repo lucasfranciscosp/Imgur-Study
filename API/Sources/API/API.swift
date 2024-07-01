372a42e29116055839d1a86692d377cb14d2cb90
Clissia Bovi <bovibc@gmail.com>
2024-07-01 09:25:11 -0300

    fixing api

--- API/Sources/API/API.swift
+++ API/Sources/API/API.swift
@@ -8,19 +8,17 @@ class API {
 
     let imgurURL = "https://api.imgur.com/"
 
-    func getMostViral(completion: @escaping (([Data]?) -> Void)){
+    func getMostViral(completion: @escaping ((Data?) -> Void)){
         let url = imgurURL + "3/gallery/hot/time/day/1"
         var urlRequest:URLRequest = URLRequest(url: URL(string: url)!)
         urlRequest.httpMethod = "GET"
-        urlRequest.setValue("Client-ID 705d1626bf00932", forHTTPHeaderField: "Authorization")
+        urlRequest.setValue("Client-ID 7bc6c559d469760", forHTTPHeaderField: "Authorization")
         urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
         let task = URLSession.shared.dataTask(with: urlRequest){data,response,error in
             print(response as Any)
             if let data = data {
                do {
-//                    let memesList:MemeList = try JSONDecoder().decode(MemeList.self, from: data)
-//                    print(memesList.data.first?.title as Any)
-//                    completion(memesList)
+                    completion(data)
                } catch let error {
                   print(error)
                }
--- Imgur-Study/Imgur-Study/MostViral/GenericButton/GenericButton.swift
+++ Imgur-Study/Imgur-Study/MostViral/GenericButton/GenericButton.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import API
 
 struct GenericButton: View {
     let color: Color
